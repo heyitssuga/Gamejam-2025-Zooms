80b0139c71b338636262aa50ad3a75151d6bd530
Allen Krupic <110694062+heyitssuga@users.noreply.github.com>
2025-09-26 22:02:11 -0600

    More camera stuff

--- GameJam 2025 Zooms/Assets/Scripts/CameraMovementScript.cs
+++ GameJam 2025 Zooms/Assets/Scripts/CameraMovementScript.cs
@@ -9,6 +9,6 @@ public class CameraMovementScript : MonoBehaviour
     {
         Vector3 targetPosition = playerTransform.position;
 
-        transform.position = new Vector3(targetPosition.x, targetPosition.y, transform.position.z);
+        transform.position = new Vector3(targetPosition.x, 0, transform.position.z);
     }
 }
