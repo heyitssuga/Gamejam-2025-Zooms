fca8180eade9b92714941d62dc3125b35697655b
Allen Krupic <110694062+heyitssuga@users.noreply.github.com>
2025-09-26 21:25:25 -0600

    Movement Better

--- GameJam 2025 Zooms/Assets/Scripts/MovementScript.cs
+++ GameJam 2025 Zooms/Assets/Scripts/MovementScript.cs
@@ -5,6 +5,8 @@ public class MovementScript : MonoBehaviour
 {
 
     private InputAction moveAction;
+    private Vector3 position;
+    public float speed = 5f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,6 +16,24 @@ public class MovementScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        this.gameObject.transform.Translate(new Vector3(moveAction.ReadValue<Vector2>().x, moveAction.ReadValue<Vector2>().y) * Time.deltaTime * 5, 0);
+        position = this.gameObject.transform.position;
+        this.gameObject.transform.Translate(new Vector3(moveAction.ReadValue<Vector2>().x, moveAction.ReadValue<Vector2>().y) * Time.deltaTime * speed, 0);
+        
+        if(position.y > 4.5f)
+        {
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, 4.5f, this.gameObject.transform.position.z);
+        } else if(position.y < -4.5f)
+        {
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, -4.5f, this.gameObject.transform.position.z);
+        }
+
+        if (position.x > 8.4f)
+        {
+            this.gameObject.transform.position = new Vector3(8.4f, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        }
+        else if (position.x < -8.4f)
+        {
+            this.gameObject.transform.position = new Vector3(-8.4f, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+        }
     }
 }
