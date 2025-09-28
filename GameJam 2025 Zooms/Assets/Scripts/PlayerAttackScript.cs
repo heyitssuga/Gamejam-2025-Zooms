2e360cdec5ce8286d624fb9e9bf6ec90227ff2e6
Allen Krupic <110694062+heyitssuga@users.noreply.github.com>
2025-09-28 02:51:29 -0600

    Thumbs up emoji I think

--- GameJam 2025 Zooms/Assets/Scripts/PlayerAttackScript.cs
+++ GameJam 2025 Zooms/Assets/Scripts/PlayerAttackScript.cs
@@ -13,6 +13,7 @@ public class PlayerAttackScript : MonoBehaviour
     [SerializeField] private Animator animator;
     [SerializeField] private AnimationClip clip, clip2, clip3;
     [SerializeField] private GameObject hitbox;
+    [SerializeField] private PlayerScript playa;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -30,88 +31,92 @@ public class PlayerAttackScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (attackCooldown <= 0)
+        if(playa.canBeAttacked)
         {
-            Debug.Log("Refreshed!");
-            hitbox.SetActive(false);
-            length = clip.length;
-            length2 = clip2.length;
-            length3 = clip3.length;
-            hit1 = false;
-            hit2 = false;
-            hit3 = false;
-            canAttack = true;
-            attackCooldown = attackTimerAtFirst;
-        }
-
-        if (hit1)
-        {
-            attackCooldown -= Time.deltaTime;
-            length -= Time.deltaTime;
-            if(length <= 0)
+            if (attackCooldown <= 0)
             {
-                animator.SetBool("IsHit1", false);
+                Debug.Log("Refreshed!");
+                hitbox.SetActive(false);
+                length = clip.length;
+                length2 = clip2.length;
+                length3 = clip3.length;
+                hit1 = false;
+                hit2 = false;
+                hit3 = false;
+                canAttack = true;
+                attackCooldown = attackTimerAtFirst;
+            }
+
+            if (hit1)
+            {
+                attackCooldown -= Time.deltaTime;
+                length -= Time.deltaTime;
+                if (length <= 0)
+                {
+                    animator.SetBool("IsHit1", false);
+                }
+            }
+
+            if (!hit2 && attackCooldown <= attackTimerAtFirst - 0.5f)
+            {
+                hitbox.SetActive(false);
+                canAttack2 = true;
+            }
+
+            if (hit2 && !canAttack2)
+            {
+                length2 -= Time.deltaTime;
+                if (length2 <= 0)
+                {
+                    animator.SetBool("IsHit2", false);
+                }
+            }
+
+            if (!hit3 && attackCooldown <= attackTimerAtFirst - 1f)
+            {
+                hitbox.SetActive(false);
+                canAttack3 = true;
+            }
+
+            if (attackCooldown <= attackTimerAtFirst - 1.5f)
+            {
+                hitbox.SetActive(false);
+            }
+
+            if (hit3 && !canAttack3)
+            {
+                length3 -= Time.deltaTime;
+                if (length3 <= 0)
+                {
+                    animator.SetBool("IsHit3", false);
+                }
+            }
+
+            if (attackAction.IsPressed() && canAttack)
+            {
+                hitbox.SetActive(true);
+                hit1 = true;
+                animator.SetBool("IsHit1", true);
+                canAttack = false;
+                Debug.Log("Attack 1");
+            }
+            else if (attackAction.IsPressed() && canAttack2)
+            {
+                hitbox.SetActive(true);
+                hit2 = true;
+                animator.SetBool("IsHit2", true);
+                canAttack2 = false;
+                Debug.Log("Attack 2");
+            }
+            else if (attackAction.IsPressed() && canAttack3)
+            {
+                hitbox.SetActive(true);
+                hit3 = true;
+                animator.SetBool("IsHit3", true);
+                canAttack3 = false;
+                Debug.Log("Attack 3");
             }
         }
-
-        if(!hit2 && attackCooldown <= attackTimerAtFirst - 0.5f)
-        {
-            hitbox.SetActive(false);
-            canAttack2 = true;
-        }
-
-        if (hit2 && !canAttack2)
-        {
-            length2 -= Time.deltaTime;
-            if (length2 <= 0)
-            {
-                animator.SetBool("IsHit2", false);
-            }
-        }
-
-        if (!hit3 && attackCooldown <= attackTimerAtFirst - 1f)
-        {
-            hitbox.SetActive(false);
-            canAttack3 = true;
-        }
-
-        if(attackCooldown <= attackTimerAtFirst - 1.5f)
-        {
-            hitbox.SetActive(false);
-        }
-
-        if(hit3 && !canAttack3)
-        {
-            length3 -= Time.deltaTime;
-            if (length3 <= 0)
-            {
-                animator.SetBool("IsHit3", false);
-            }
-        }
-
-        if (attackAction.IsPressed() && canAttack)
-        {
-            hitbox.SetActive(true);
-            hit1 = true;
-            animator.SetBool("IsHit1", true);
-            canAttack = false;
-            Debug.Log("Attack 1");
-        }
-       else if(attackAction.IsPressed() && canAttack2)
-        {
-            hitbox.SetActive(true);
-            hit2 = true;
-            animator.SetBool("IsHit2", true);
-            canAttack2 = false;
-            Debug.Log("Attack 2");
-        }
-        else if (attackAction.IsPressed() && canAttack3)
-        {
-            hitbox.SetActive(true);
-            hit3 = true;
-            animator.SetBool("IsHit3", true);
-            canAttack3 = false;
-            Debug.Log("Attack 3");
-        }
+        
     }
 }
--- GameJam 2025 Zooms/Assets/Scripts/PlayerScript.cs
+++ GameJam 2025 Zooms/Assets/Scripts/PlayerScript.cs
@@ -8,8 +8,10 @@ public class PlayerScript : MonoBehaviour
     public float hp = 5;
     public SimpleFlash flashDamage;
     private float timer;
-    private bool canBeAttacked = true;
+    public bool canBeAttacked = true;
     [SerializeField] private GameObject player;
+    [SerializeField] private Animator animator;
+    [SerializeField] private Sprite[] sprites;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,18 +22,28 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(hp <= 0)
-        {
-            Destroy(player);
-        }
+        //if(hp <= 0)
+        //{
+        //    Destroy(player);
+        //}
         if(!canBeAttacked)
         {
+            player.GetComponent<Animator>().enabled = false;
+            player.GetComponent<SpriteRenderer>().sprite = sprites[2];
             isAttacked = false;
             timer -= Time.deltaTime;
             if (timer <= 0)
             {
                 canBeAttacked = true;
                 timer = 2f;
+                player.GetComponent<Animator>().enabled = true;
+                if (!animator.GetBool("IsMoving"))
+                {
+                    player.GetComponent<SpriteRenderer>().sprite = sprites[0];
+                } else
+                {
+                    player.GetComponent<SpriteRenderer>().sprite = sprites[1];
+                }
             }
         }
         if (canBeAttacked)
@@ -44,7 +56,6 @@ public class PlayerScript : MonoBehaviour
     {
         if (isAttacked == true)
         {
-            rb.linearVelocity = new Vector2(0, 0);
             flashDamage.Flash();
             hp -= 1;
             isAttacked = false;
