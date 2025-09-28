51d186c9fc5f4daf661796909a0164294026cf09
Allen Krupic <110694062+heyitssuga@users.noreply.github.com>
2025-09-28 01:42:44 -0600

    I-frames

--- GameJam 2025 Zooms/Assets/Scripts/PlayerScript.cs
+++ GameJam 2025 Zooms/Assets/Scripts/PlayerScript.cs
@@ -7,16 +7,31 @@ public class PlayerScript : MonoBehaviour
     private Rigidbody2D rb;
     public float hp = 5;
     public SimpleFlash flashDamage;
+    private float timer;
+    private bool canBeAttacked = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        timer = 2f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        InflictDamage();
+        if(!canBeAttacked)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                canBeAttacked = true;
+                timer = 2f;
+            }
+        }
+        if (canBeAttacked)
+        {
+            InflictDamage();
+        }
     }
 
     private void InflictDamage()
@@ -27,6 +42,7 @@ public class PlayerScript : MonoBehaviour
             flashDamage.Flash();
             hp -= 1;
             isAttacked = false;
+            canBeAttacked = false;
         }
 
     }
